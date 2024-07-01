bef8e1a36926ecb1dd6a175b8865c104d8973a6d
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-07-01 16:39:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventListener('click',async ()=>{
+	var txt=document.getElementById('text').value;
+	var delay=document.getElementById('delay').value;
+	await delayFunction(delay);
+	document.getElementById('output').innerText = txt;
+});
+
+function delayFunction(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
